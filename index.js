e2a7e94769b4b5ece2b12a375d3c40ef4667b805
Daniel Brazel <dbrazel4@gmail.com>
2018-01-11 01:15:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var recipes = {re1: 'I am not a recipe'};
+
+function updateObjectWithKeyAndValue(objectName, key, value){
+  var newObj = Object.assign({}, objectName, {[key]: value})
+  return newObj
+}
+
